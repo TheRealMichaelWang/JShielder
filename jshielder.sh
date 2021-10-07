ee80f3edd6740c460430c0da893821df74d05519
Michael Wang <44185024+TheRealMichaelWang@users.noreply.github.com>
2021-10-07 16:24:00 -0700

    Only ubuntu 18 is used and debain also uses 18

--- jshielder.sh
+++ jshielder.sh
@@ -19,6 +19,8 @@
 
 #Credits to Center for Internet Security CIS
 
+#Modified for CyberPatriots
+
 ##############################################################################################################
 
 f_banner(){
@@ -52,47 +54,10 @@ else
       f_banner
 fi
 
-
-menu=""
-until [ "$menu" = "10" ]; do
-
-clear
-f_banner
-
-echo
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m SELECT YOUR LINUX DISTRIBUTION"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "1. Ubuntu Server 16.04 LTS"
-echo "2. Ubuntu Server 18.04 LTS"
-echo "3. Linux CentOS 7 (Coming Soon)"
-echo "4. Debian GNU/Linux 8 (Coming Soon)"
-echo "5. Debian GNU/Linux 9 (Coming Soon)"
-echo "6. Red Hat Linux 7 (Coming Soon)"
-echo "7. Exit"
-echo
-
-read menu
-case $menu in
-
-1)
-cd UbuntuServer_16.04LTS/
-chmod +x jshielder.sh
-./jshielder.sh
-;;
-
-2)
 cd UbuntuServer_18.04LTS/
 chmod +x jshielder.sh
 ./jshielder.sh
 ;;
-
-8)
-break
-;;
-
-*) ;;
 
 esac
 done
